e4a76beb45b6b2aac34d3d718b216e07487b27bf
Jconners23 <john.connolly@ada.ac.uk>
2022-11-04 12:38:26 +0000

    make guess function created, test integer implemented

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let num = 0;
-
 function generateRandomNumber() {
   let rnd = Math.floor(Math.random() * 100) + 1;
   //random number between 1 and 100
@@ -15,3 +13,15 @@ function differenceFromAnswer(guess, answer) {
     return "Correct";
   }
 }
+
+function makeAGuess(answer) {
+  let guess = prompt("Enter your guess ");
+  if ((/^\d+$/).test(guess)) {
+    return differenceFromAnswer(guess, answer);
+  } else {
+    return "Not an integer";
+  }
+}
+
+let num = generateRandomNumber();
+console.log(makeAGuess(num));
